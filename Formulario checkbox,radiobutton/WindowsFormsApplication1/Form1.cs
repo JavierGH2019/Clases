30fd93260a0e63dc3b82c885818654a508bd7b98
JavierGH2019 <47840404+JavierGH2019@users.noreply.github.com>
2019-03-28 23:30:11 -0600

    Add files via upload
    Propiedades y eventos de formulario

---
+++ Formulario checkbox,radiobutton/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex==0){
+                label1.BackColor = Color.Blue;
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                label1.BackColor = Color.Red;
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                label1.BackColor = Color.Green;
+                this.BackColor = Color.SkyBlue;
+                
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = 0; i < 10;i++ )
+            {
+                comboBox1.Items.Add(i.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+    }
+}
